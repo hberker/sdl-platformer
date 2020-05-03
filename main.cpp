8b2d39f78a34bebeca7a511307729952fc1c531a
hberker <hosni.berker@gmail.com>
2020-05-02 20:56:10 -0400

    adds logic for player rendering

--- main.cpp
+++ main.cpp
@@ -1,45 +1,52 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
 #include <SDL2/SDL.h>
 #include <iostream>
 
+#include "include/constant.hpp"
 #include "include/WindowRenderer.hpp"
 #include "include/Tile.hpp"
-#include "include/level.hpp"
-
-constexpr unsigned int TILES_HIGH = 20;
-constexpr unsigned int TILES_WIDE = 20;
-
-constexpr unsigned int WINDOW_WIDTH = TILE_SIZE * TILES_WIDE;
-constexpr unsigned int WINDOW_HEIGHT = TILE_SIZE * TILES_HIGH;
-
-
-
+#include "include/utility.hpp"
+#include "include/Player.hpp"
 
 int main(int argc, char **argv)
 {
     bool game_running = true;
     SDL_Event event;
     WindowRenderer window("test",WINDOW_WIDTH,WINDOW_HEIGHT);
+    SDL_Rect cam = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
 
-    SDL_Texture * rock_texture = window.load_texture("assets/rock_tile.png");
+    Tile *tiles[TOTAL_TILES];
 
-    Tile rock_tile(0,0, rock_texture);
+    SDL_Texture * tile_textures = window.load_texture("assets/tiles.png");
+    SDL_Texture * dot_texture = window.load_texture("assets/white_ball.png");
+
+    Player player(dot_texture);
+    load_tiles(tiles);
 
     while(game_running)
     {
+        window.clear();
+
         while(SDL_PollEvent(&event))
         {
             if(event.type == SDL_QUIT) game_running = false;
+            player.handle_events(event);
         }
-        for(int i = 0; i < WINDOW_HEIGHT; i+=50)
+        
+        for(int i = 0;  i < TOTAL_TILES; i++)
         {
-            for(int j = 0; j < WINDOW_HEIGHT; j+=50)
-            {
-                rock_tile = Tile(i,j, rock_texture);
-                window.render(rock_tile); 
-            }
+            window.render_tile(tiles[i], cam, tile_textures);
         }
+        player.move_player(tiles);
+        player.set_camera(cam);
+
+        window.render_player(player,cam);
         
         window.display();
 
     }
+    close(tiles);
 }
+#endif
